feaf3fd2add9f75e379a26cf61fce69131c96390
KloopRE <100780612+KloopRE@users.noreply.github.com>
2024-04-03 19:54:40 +0300

    merge

--- views/product/cart.php
+++ views/product/cart.php
@@ -29,12 +29,8 @@
                     <h1 class="product-title"><?php echo $products[$i]['name']; ?></h1>
                     <div class="product-price"><?php echo $products[$i]['price']; ?></div>
                 </div>
-<<<<<<< Updated upstream
                 <p class="brand-name"><?php echo $products[$i]['brand']; ?></p>
-=======
-                <p class="brand-name"><?php echo $product['brand']; ?></p>
-                <textarea class="brand-name" name="productId" id="productId">товар: <?php echo $product['id']; ?></textarea>
->>>>>>> Stashed changes
+                <textarea class="brand-name" name="productId" id="productId">товар: <?php echo $products[$i]['id']; ?></textarea>
                 <div class="selected-size">
                     <?php foreach ($products[$i]['sizes'] as $size): ?>
                         <input id="size-<?php echo $products[$i]['id'] . '-' . $size; ?>"
@@ -48,14 +44,9 @@
                     <div class="buttons">
                         <?php if ($isCatalog): ?>
                             <?php if (isset($_SESSION['login_success']) && $_SESSION['login_success'] === true): ?>
-<<<<<<< Updated upstream
-                                <button onclick="addToCart(<?php echo $products[$i]['id']; ?>)" class="add-to-cart"
-                                        type="button" title="Добавить в корзину">
+                                <button type="submit" class="button" title="Добавить в корзину">Добавить в корзину</button>
                                     <i class="fas fa-shopping-cart"></i>
                                 </button>
-=======
-                                <button type="submit" class="button" title="Добавить в корзину">Добавить в корзину</button>
->>>>>>> Stashed changes
                             <?php else: ?>
                                 <button onclick="redirectToRegistration()" class="add-to-cart" type="button"
                                         title="Добавить в корзину">
@@ -75,36 +66,6 @@
                         window.location.href = '/MVC-site/access';
                     }
 
-                    function addToCart(productId) {
-                        $.ajax({
-                            type: 'POST',
-                            url: '/MVC-site/addToCart',
-                            data: {productId: productId},
-                            success: function (response) {
-                                alert('Товар добавлен в корзину!');
-                            },
-                            error: function (xhr, status, error) {
-                                alert('Ошибка при добавлении товара в корзину: ' + error);
-                            }
-                        });
-                    }
-
-                    function deleteFromCart(productId) {
-                        // Отправка запроса на сервер для удаления товара из корзины
-                        $.ajax({
-                            type: 'POST',
-                            url: '/MVC-site/cart',
-                            data: {productId: productId},
-                            success: function (response) {
-                                // Обработка успешного удаления товара
-                                alert('Товар удален из корзины!');
-                            },
-                            error: function (xhr, status, error) {
-                                // Обработка ошибки удаления товара
-                                alert('Ошибка при удалении товара из корзины: ' + error);
-                            }
-                        });
-                    }
                 </script>
             </form>
         </div>
